fca755db5e7ed6bafc28b96f9ead5fbaab591a63
UCHINO Yuki <160313760+UCHINO-Yuki@users.noreply.github.com>
2024-03-06 18:21:28 +0900

    Add files via upload

--- DDclass/@dd/Info.m
+++ DDclass/@dd/Info.m
@@ -8,7 +8,7 @@ function Info
 %   revised ... 2024-03-06 ... UCHINO Yuki
 fprintf('\n----------------------------------------------------\n');
 fprintf(' DDclass - A double-double class toolbox for MATLAB\n');
-fprintf(' Version : 1.0.1\n');
+fprintf(' Version : 1.0.2\n');
 fprintf(' Release : 2024-03-06\n');
 fprintf(' (c) 2024 UCHINO Yuki\n');
 fprintf('----------------------------------------------------\n\n');
