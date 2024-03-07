2cea38c15607303bbe2be9785cad43d412728051
UCHINO Yuki <160313760+UCHINO-Yuki@users.noreply.github.com>
2024-03-08 07:43:59 +0900

    Add files via upload

--- DDclass/startDD.m
+++ DDclass/startDD.m
@@ -3,9 +3,10 @@ function startDD
 %
 %   Adapt this to your local needs.
 %
-%   See also DD, INFO
+%   See also DD, DD.INFO
 %
 %   written ... 2024-02-23 ... UCHINO Yuki
+%   revised ... 2024-03-08 ... UCHINO Yuki
 
 err = false;
 disp(' ')
@@ -23,26 +24,54 @@ if ~err
 end
 
 disp(' ')
-disp('Checking GPU devices.')
-CheckGPU = gpuDeviceCount("available");
-if CheckGPU>0
-    disp('... Found supported GPU devices.')
-    while 1
-        x = input('... (Re-)Compile cu files? [y/n]','s');
-        if strcmpi(x,'y') || strcmpi(x,'n')
-            break;
-        end
-    end
-    if strcmpi(x,'y')
-        p2 = CompileMEXCUDA;
-        if p2
-            disp('... mexcuda succeeded.');
-        else
-            disp('... mexcuda failed.');
-        end
-    end
+disp('Checking the environment.')
+try
+    checkRelease = isMATLABReleaseOlderThan("R2021b");
+catch
+    disp('... MATLAB R2021b or later is required.');
+    return
+end
+if checkRelease
+    disp('... MATLAB R2021b or later is required.');
+    return
+end
+checkVer = ver;
+checkFPD = any(matches({checkVer.Name},'Fixed-Point Designer'));
+if ~checkFPD
+    disp('... "Fixed-Point Designer" toolbox is required.');
+    return
+end
+checkPCT = any(matches({checkVer.Name},'Parallel Computing Toolbox'));
+if ~checkPCT
+    disp('... "Parallel Computing Toolbox" toolbox is not installed.');
+    disp('... gpuArray cannot be used.')
 else
-    disp('... Unable to find a supported GPU device.');
+    disp('... No problems.');
+end
+
+if checkPCT
+    disp(' ')
+    disp('Checking GPU devices.')
+    CheckGPU = gpuDeviceCount("available");
+    if CheckGPU>0
+        disp('... Found supported GPU devices.')
+        while 1
+            x = input('... (Re-)Compile cu files? [y/n]','s');
+            if strcmpi(x,'y') || strcmpi(x,'n')
+                break;
+            end
+        end
+        if strcmpi(x,'y')
+            p2 = CompileMEXCUDA;
+            if p2
+                disp('... mexcuda succeeded.');
+            else
+                disp('... mexcuda failed.');
+            end
+        end
+    else
+        disp('... Unable to find a supported GPU device.');
+    end
 end
 
 disp(' ')
